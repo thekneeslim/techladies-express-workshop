d8adf0898dcb5de48d57a055c56eeeff5abadd4a
Denise <denise.lim.sh@gmail.com>
2021-08-11 03:32:56 +0800

    Update console output upon application start up

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Library Application listening at http://localhost:${port}`)
 });
