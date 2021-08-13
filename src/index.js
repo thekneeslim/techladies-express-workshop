b3c420fe71a1f2e780b892e0db5372b7b31e4018
Denise <denise.lim.sh@gmail.com>
2021-08-13 22:19:23 +0800

    Introduce GET endpoint to /books to retrieve catalogue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import BookRepository from "./repository/book-repository.js";
 
 const app = express();
 const port = 3000;
 
+const booksRepository = new BookRepository();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/books', (req, res) => {
+  res.json(booksRepository.getAll());
 });
 
 app.listen(port, () => {
